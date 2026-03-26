d2e56f039694dca36eed30255405ea77aec58808
fabasapro-reserve <fabasapro@hotmail.com>
2026-03-26 16:32:14 -0300

    Primeiro commit: joguei tudo pro Git

---
+++ automatic-click-bot-tool-vb/Form1.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(86, 82)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(300, 197)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Automatic Click Bot Tool + Esc to Cancel"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+End Class
---
+++ automatic-click-bot-tool-vb/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Windows.Forms
+Imports System.Drawing
+Imports System.Threading
+Public Class Form1
+    Private cts As CancellationTokenSource
+    ''' <summary>
+    ''' Move o cursor de um ponto A(X,Y) para um ponto B(X,Y).
+    ''' </summary>
+    ''' <param name="pointA">Posição A.</param>
+    ''' <param name="pointB">Posição B.</param>
+    ''' <param name="token">Token para cancelar.</param>
+    Private Async Function SmoothCursorMoveAsync(pointA As Point, pointB As Point, token As CancellationToken) As Task
+        Dim steps As Integer = 75    ' (opção) quanto maior, mais suave
+        Dim delay As Integer = 5     ' (opção) milissegundos entre passos
+        Dim dx As Double = (pointB.X - pointA.X) / steps
+        Dim dy As Double = (pointB.Y - pointA.Y) / steps
+        Dim currentX As Double = pointA.X
+        Dim currentY As Double = pointA.Y
+        For i As Integer = 1 To steps
+            token.ThrowIfCancellationRequested()
+            currentX += dx
+            currentY += dy
+            Cursor.Position = New Point(CInt(currentX), CInt(currentY))
+            Await Task.Delay(delay, token)
+        Next
+    End Function
+    Protected Overrides Function ProcessCmdKey(ByRef msg As Message, keyData As Keys) As Boolean
+        If keyData = Keys.Escape Then
+            If cts IsNot Nothing Then
+                cts.Cancel()
+            End If
+            Return True
+        End If
+        Return MyBase.ProcessCmdKey(msg, keyData)
+    End Function
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cts = New CancellationTokenSource()
+        Try
+            Await SmoothCursorMoveAsync(New Point(230, 240), New Point(800, 600), cts.Token)
+        Catch ex As OperationCanceledException
+            MessageBox.Show("A tecla <Esc> foi pressionada: o movimento do cursor foi cancelado!", "Automatic Click Bot Tool", MessageBoxButtons.OK)
+        End Try
+    End Sub
+End Class
